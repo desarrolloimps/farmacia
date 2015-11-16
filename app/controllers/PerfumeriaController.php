b3318cd49bf2ae4e6e5e86f70a20960aa2f33bc3
munozdaniel <munozda87@hotmail.com>
2015-11-16 13:44:33 -0300

    Seccion Perfumeria

---
+++ app/controllers/PerfumeriaController.php
@@ -0,0 +1,16 @@
+<?php
+
+class PerfumeriaController extends ControllerBase
+{
+    public function initialize()
+    {
+        $this->tag->setTitle('Perfumeria');
+        parent::initialize();
+
+    }
+    public function indexAction()
+    {
+
+    }
+}
+
