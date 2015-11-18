2863852f39e414b1500d6ce038b3f45ab5170b8b
munozdaniel <munozda87@hotmail.com>
2015-11-18 14:38:07 -0300

    Agregado los controladores Farmacia y Convenios.

---
+++ app/controllers/ConveniosController.php
@@ -0,0 +1,17 @@
+<?php
+
+class ConveniosController extends ControllerBase
+{
+    public function initialize()
+    {
+        $this->tag->setTitle('Convenios');
+        parent::initialize();
+
+    }
+    public function indexAction()
+    {
+
+    }
+
+}
+
---
+++ app/controllers/FarmaciaController.php
@@ -0,0 +1,16 @@
+<?php
+
+class FarmaciaController extends ControllerBase
+{
+    public function initialize()
+    {
+        $this->tag->setTitle('Nuestra Farmacia');
+        parent::initialize();
+
+    }
+    public function indexAction()
+    {
+
+    }
+}
+
